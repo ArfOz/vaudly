9c09f9c17f1ad0a6ae3dd5c2321df2acd8e896a5
Arif Trends <arfoz1245@gmail.com>
2025-10-15 23:55:44 +0200

    chore: update dependencies and add eas-build post-install script
    - Updated various dependencies in package.json, including @nestjs, expo, and nx packages.
    - Added new devDependencies for testing and build tools.
    - Introduced a new script (eas-build-post-install.mjs) to create a symlink from the project's node_modules to the workspace's node_modules for EAS Build compatibility.

--- apps/backend-e2e/jest.config.ts
+++ apps/backend-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'vaudly-e2e',
+  displayName: 'backend-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/vaudly-e2e',
+  coverageDirectory: '../../coverage/backend-e2e',
 };
